5b98033779737499bb4e2fc8b3ffdca3c5e13910
Natalia <natsantosmartinez@gmail.com>
2025-04-14 11:21:42 +0200

    Todos los cambios sin los videos

---
+++ languages/lang.js
@@ -0,0 +1,128 @@
+const translations = {
+    es: {
+      portfolio: "Mi portafolio", 
+      page_title: "Portafolio de Desarrollador Web",
+      job_title: "Ingeniera de Software",
+      see_projects: "Ver Proyectos",
+      about_me: "Sobre Mí",
+      about_me_text: `Soy Ingeniera Informática, especializada en Ingeniería del Software, y recientemente he finalizado el Máster en Desarrollo de Aplicaciones y Servicios Web (Full Stack). Me encanta el desarrollo de software y todo lo que implique aprender, mejorar y construir soluciones que realmente funcionen y aporten valor.<br><br>
+      Durante mis prácticas en empresa, trabajé en dos empresas: una centrada en el frontend de aplicaciones web, donde me enfoqué en mejorar la interfaz y experiencia del usuario, y otra en el desarrollo de un proyecto de escritorio en Java, donde profundicé en el diseño y desarrollo de aplicaciones standalone.<br><br>
+      Actualmente formo parte del equipo de Getronics como Software Engineer, donde me encargo de resolver incidencias, mejorar sistemas existentes y desarrollar nuevas funcionalidades para distintas aplicaciones web.<br><br>    
+      Además, he creado diversas aplicaciones tanto en equipo como de forma autónoma durante mis estudios de universidad y máster. <strong>¡Te invito a ver alguno de estos proyectos en este portfolio!</strong>`,
+      contact: "Contacto",
+      contact_me: "¿Quieres ponerte en contacto conmigo? Rellena el formulario:", 
+      name: "Nombre", 
+      email: "Correo electrónico", 
+      message: "Mensaje", 
+      send: "Enviar", 
+      get_to_know_me: "¡Conóceme!",
+      my_skills: "Mis habilidades", 
+      projects: "Proyectos", 
+      web_applications: "Aplicaciones web", 
+      mobile_applications: "Aplicaciones móviles", 
+      homemate_description: "Aplicación web para facilitar la búsqueda de habitaciones en alquiler y de compañeros de piso en España.", 
+      biblioconnect_description: "Aplicación web para mejorar el acceso y gestión de libros en formato físico en las bibliotecas públicas de la Comunidad Valenciana.", 
+      sportpower_description: "Aplicación web de una tienda que permite la compra de artículos deportivos y suplementos",
+      spendtogether_description: "Aplicación móvil para gestionar gastos compartidos de un grupo de personas de forma equitativa.", 
+      rhythmmad_description: "Aplicación móvil que permite buscar eventos programados en Madrid.", 
+      getpet_description: "Aplicación móvil para facilitar la adopción de animales", 
+      all_rights_reserved: "Todos los derechos reservados.", 
+
+      //HOMEMATE
+      demo_project: "Demo proyecto",
+      play_demo_info: "En esta sección puedes ver una demostración de cómo funciona la aplicación HomeMate. Haz clic en el botón 'Reproducir Demo' para ver una presentación en vivo del funcionamiento de la plataforma y sus principales características.", 
+      play_demo_button: "Reproducir Demo", 
+      description: "Descripción", 
+      description_app_homemate: `Aplicación web que facilita el proceso de encontrar alojamiento compartido y compañeros de piso en
+      España. La creciente demanda de este tipo de soluciones, debido a los elevados precios
+      del alquiler y la necesidad de compartir gastos, ha mostrado que los métodos tradicionales,
+      como las redes sociales o los tablones de anuncios, no siempre son suficientes.<br></br>
+      HomeMate se concibe como una alternativa innovadora, que centraliza las ofertas de habitaciones 
+      y los perfiles de posibles compañeros de piso en una única plataforma. La aplicación
+      se ha desarrollado utilizando tecnologías modernas como React para el frontend, NodeJS
+      para el backend y Firebase como base de datos. Además, integra herramientas como Cloudinary 
+      para la gestión de imágenes, OpenStreetMap y Leaflet para la visualización de mapas
+      y la sincronización con Google Calendar para la gestión de visitas.<br></br> 
+      El desarrollo del proyecto se ha basado en metodologías ágiles, organizando el trabajo en
+      sprints y utilizando Trello para la gestión de tareas. Los resultados obtenidos incluyen funcionalidades
+      clave como la publicación y gestión de anuncios de habitaciones, la consulta de
+      perfiles de compañeros de piso, un sistema de chat para la comunicación entre usuarios, y
+      la programación de visitas con integración en Google.`,
+      technologies: "Tecnologías Utilizadas", 
+      project_documentation: "Documentación del proyecto", 
+      project_documentation_info: "En esta sección podrás descargar los documentos relevantes del proyecto, como la memoria técnica en formato PDF y las diapositivas de la presentación en formato PPTX. Haz clic en los enlaces para obtener los archivos correspondientes.", 
+      download_documentation: "Descargar Documentación (PDF)", 
+      download_slides: "Descargar Diapositivas (PDF)"
+
+
+    },
+    en: {
+      portfolio: "My portfolio", 
+      page_title: "Web Developer Portfolio",
+      job_title: "Software Engineer",
+      see_projects: "View Projects",
+      about_me: "About Me",
+      about_me_text: `I am a Computer Engineer specialized in Software Engineering, and I recently completed a Master’s Degree in Web Applications and Services Development (Full Stack). I’m passionate about software development and everything that involves learning, improving, and building solutions that truly work and provide value.<br><br>
+      During my internships, I worked at two different companies: one focused on the frontend of web applications, where I specialized in enhancing the interface and user experience, and another dedicated to developing a desktop project in Java, where I deepened my skills in designing and building standalone applications.<br><br>
+      I am currently part of the Getronics team as a Software Engineer, where I handle incident resolution, improve existing systems, and develop new functionalities for various web applications.<br><br>
+      Additionally, I have created several applications both in teams and independently throughout my university and master’s studies. <strong>I invite you to check out some of these projects in this portfolio!</strong>`,
+      contact: "Contact",
+      contact_me: "Want to get in touch? Fill out the form:", 
+      name: "Name", 
+      email: "Email", 
+      message: "Message", 
+      send: "Send", 
+      get_to_know_me: "Get to know me!",
+      my_skills: "My skills", 
+      projects: "Projects", 
+      web_applications: "Web applications", 
+      mobile_applications: "Mobile applications", 
+      homemate_description: "Web application to facilitate the search for rental rooms and roommates in Spain.",
+      biblioconnect_description: "Web application to improve access to and management of physical books in public libraries in the Valencian Community.",
+      sportpower_description: "Web application for a store that allows the purchase of sports equipment and supplements.",
+      spendtogether_description: "Mobile application to manage shared expenses among a group of people in a fair way.",
+      rhythmmad_description: "Mobile application that allows users to find scheduled events in Madrid.",
+      getpet_description: "Mobile application to facilitate animal adoption.", 
+      all_rights_reserved: "All rights reserved.", 
+
+      //HOMEMATE
+      demo_project: "Demo Project",
+      play_demo_info: "In this section, you can watch a demonstration of how the HomeMate application works. Click the 'Play Demo' button to see a live presentation of the platform's functionality and main features.",
+      play_demo_button: "Play Demo",
+      description: "Description",
+      description_app_homemate: `Web application that facilitates the process of finding shared accommodation and roommates in Spain.
+      The growing demand for this type of solution, due to high rental prices and the need to share expenses,
+      has shown that traditional methods such as social networks or bulletin boards are not always sufficient.<br></br>
+      HomeMate is conceived as an innovative alternative that centralizes room listings and potential roommate profiles 
+      on a single platform. The application has been developed using modern technologies such as React for the frontend, 
+      NodeJS for the backend, and Firebase as the database. It also integrates tools like Cloudinary for image management, 
+      OpenStreetMap and Leaflet for map visualization, and synchronization with Google Calendar for visit scheduling.<br></br>
+      The project development was based on agile methodologies, organizing the work in sprints and using Trello for task management. 
+      The results include key features such as posting and managing room ads, browsing roommate profiles, a chat system for user communication, 
+      and visit scheduling with Google integration.`,
+      technologies: "Technologies Used",
+      project_documentation: "Project Documentation",
+      project_documentation_info: "In this section, you can download the relevant project documents, such as the technical report in PDF format and the presentation slides in PPTX format. Click the links to download the corresponding files.",
+      download_documentation: "Download Documentation (PDF)",
+      download_slides: "Download Slides (PDF)"
+
+    }
+  };
+  
+  function translatePage(lang) {
+    // Traduce texto plano (textContent)
+    document.querySelectorAll("[data-i18n]").forEach((el) => {
+      const key = el.getAttribute("data-i18n");
+      if (translations[lang] && translations[lang][key]) {
+        el.textContent = translations[lang][key];
+      }
+    });
+  
+    // Traduce contenido HTML (innerHTML)
+    document.querySelectorAll("[data-i18n-html]").forEach((el) => {
+      const key = el.getAttribute("data-i18n-html");
+      if (translations[lang] && translations[lang][key]) {
+        el.innerHTML = translations[lang][key];
+      }
+    });
+  }
